26ce04e81e3b1238bce19ff806c90af9b21b433b
andy- Mac <andrew.zhou93@gmail.com>
2022-01-14 16:58:25 -0500

    feature: push initial data to mongodb

--- app.js
+++ app.js
@@ -4,9 +4,37 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-async function scrape() {
-  let scrapedPackage = [];
+const now = new Date();
 
+let scrapedPackage = [];
+
+let currentPackage = {
+  solar_wind_speed: 0,
+  solar_mag_field_1: 1,
+  solar_mag_field_2: 2,
+  radio_flux: 3,
+  yester_max_radio_blackout: 4,
+  yester_max_solar_radiation: 5,
+  yester_max_geomag_storm: 6,
+  current_radio_blackout: 7,
+  current_solar_radiation: 8,
+  current_geomag_storm: 9,
+  predict_day1_geomag_storm: 10,
+  predict_day2_geomag_storm: 11,
+  predict_day3_geomag_storm: 12,
+  predict_day1_minor_radio_blackout: 13,
+  predict_day2_minor_radio_blackout: 14,
+  predict_day3_minor_radio_blackout: 15,
+  predict_day1_major_radio_blackout: 16,
+  predict_day2_major_radio_blackout: 17,
+  predict_day3_major_radio_blackout: 18,
+  predict_day1_solar_storm: 19,
+  predict_day2_solar_storm: 20,
+  predict_day3_solar_storm: 21,
+  scrape_time: now,
+};
+
+async function scrape() {
   try {
     const browser = await puppeteer.launch({
       headless: true,
@@ -33,7 +61,8 @@ async function scrape() {
       const item = await page.evaluate((elem) => elem.innerHTML, elementHandle);
 
       // Simple Tests
-      console.log(`Summary-item:`, item);
+      // console.log(`Summary-item:`, item);
+      scrapedPackage.push(item);
 
       // Test length, count, dates
       // console.log("length: ", solarWindStatsHandles.length);
@@ -56,7 +85,8 @@ async function scrape() {
       const item = await page.evaluate((elem) => elem.innerHTML, elementHandle);
 
       // Simple Tests
-      console.log(`Description:`, item);
+      // console.log(`Description:`, item);
+      scrapedPackage.push(item);
 
       // Test length, count, dates
       // console.log("length: ", scaleDescriptionHandles.length);
@@ -77,8 +107,8 @@ async function scrape() {
       const item = await page.evaluate((elem) => elem.innerHTML, elementHandle);
 
       // Simple Tests
-      console.log(item, "chance of Minor Radio Blackout");
-
+      // console.log(item, "chance of Minor Radio Blackout");
+      scrapedPackage.push(item);
       // Test length, count, dates
       // console.log("length: ", minorProbHandles.length);
       // console.log(minorProbCount);
@@ -98,7 +128,8 @@ async function scrape() {
       const item = await page.evaluate((elem) => elem.innerHTML, elementHandle);
 
       // Simple Tests
-      console.log(item, "chance of Major Radio Blackout");
+      // console.log(item, "chance of Major Radio Blackout");
+      scrapedPackage.push(item);
 
       // Test length, count, dates
       // console.log("length: ", majorProbHandles.length);
@@ -119,7 +150,8 @@ async function scrape() {
       const item = await page.evaluate((elem) => elem.innerHTML, elementHandle);
 
       // Simple Tests
-      console.log(item, "chance of Solar Storm Blackout");
+      // console.log(item, "chance of Solar Storm");
+      scrapedPackage.push(item);
 
       // Test length, count, dates
       // console.log("length: ", solarStormProbHandles.length);
@@ -133,14 +165,57 @@ async function scrape() {
       // }
     }
 
+    // Replace placeholders in current Package
+    for (key in currentPackage) {
+      for (let i = 0; i < scrapedPackage.length; i++) {
+        if (i === currentPackage[key]) {
+          currentPackage[key] = scrapedPackage[i];
+          // console.log(currentPackage);
+        }
+      }
+    }
+    for (key in currentPackage) {
+      if (
+        key === "solar_wind_speed" ||
+        key === "solar_mag_field_1" ||
+        key === "solar_mag_field_2" ||
+        key === "radio_flux"
+      ) {
+        console.log("match_nums: ", key, currentPackage[key]);
+        currentPackage[key] = Number(currentPackage[key]);
+      } else if (
+        key === "predict_day1_minor_radio_blackout" ||
+        key === "predict_day2_minor_radio_blackout" ||
+        key === "predict_day3_minor_radio_blackout" ||
+        key === "predict_day1_major_radio_blackout" ||
+        key === "predict_day2_major_radio_blackout" ||
+        key === "predict_day3_major_radio_blackout" ||
+        key === "predict_day1_solar_storm" ||
+        key === "predict_day2_solar_storm" ||
+        key === "predict_day3_solar_storm"
+      ) {
+        // console.log("match_%s: ", key, currentPackage[key]);
+        const percentRemoved = currentPackage[key].replace("%", "");
+        // console.log(percentRemoved);
+        currentPackage[key] = Number(percentRemoved);
+      }
+    }
+
+    console.log("Object to be sent", currentPackage);
+
+    // Call to MongoDB
+    sendData().catch(console.error);
+
     await browser.close();
   } catch (error) {}
+
+  // console.log("direct from NOAA", scrapedPackage);
 }
 // Production 30 min = 1,800,000 ms
 // const interval30Min = setInterval(() => scrape(), 1800000);
 
 // Instant Test
-// scrape();
+scrape();
 
 // Interval Test 15 secs = 15,000ms
 // const interval15Sec = setInterval(() => scrape(), 15000);
@@ -156,19 +231,13 @@ async function sendData() {
     // In-line listDatabases Test
     // const databasesList = await client.db().admin().listDatabases();
     // console.log(databasesList);
-    await createForecast(client, {
-      "-1-MaxRadioBlackout": "minor",
-      "-1-MaxSolarRadiation": "none",
-      "-1-MaxGeomagStorm": "severe",
-    });
+    await createForecast(client, currentPackage);
   } catch (err) {
     console.error(err);
   } finally {
     await client.close();
   }
 }
-
-sendData().catch(console.error);
 
 async function createForecast(client, newForecast) {
   const result = await client
